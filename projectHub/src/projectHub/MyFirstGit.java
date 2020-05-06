7c468fe39d9ed7224e9d39408ea5edd55ef51259
diptimaddiwar <diptimaddiwar@gmail.com>
2020-05-06 15:40:55 -0400

    Changes made to MyFirstGit

--- projectHub/src/projectHub/MyFirstGit.java
+++ projectHub/src/projectHub/MyFirstGit.java
@@ -5,7 +5,7 @@ public class MyFirstGit {
 	public static void main(String[] args) {
 
 System.out.println("I am inside MyFirstGit");
-
+System.out.println("Please accepts my changes in MyFirst Git");
 	}
 
 }
