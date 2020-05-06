839eb3bf3cf35eb1f471deea2e6a4da116c2dc41
diptimaddiwar <diptimaddiwar@gmail.com>
2020-05-06 15:37:39 -0400

    Entering two files to git

---
+++ projectHub/src/projectHub/MyFirstGit.java
@@ -0,0 +1,11 @@
+package projectHub;
+
+public class MyFirstGit {
+
+	public static void main(String[] args) {
+
+System.out.println("I am inside MyFirstGit");
+
+	}
+
+}
---
+++ projectHub/src/projectHub/MySecondGit.java
@@ -0,0 +1,10 @@
+package projectHub;
+
+public class MySecondGit {
+
+	public static void main(String[] args) {
+
+System.out.println("I am inside MySecondGit");
+	}
+
+}
